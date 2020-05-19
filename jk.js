15a6e638d3a7b70dd4638037f1b40d2273d0e7f0
Juha Kaikko <jkk79.jee@gmail.com>
2020-05-19 05:14:31 +0300

    moved images back to the project, imgur hosting seems bad idea

---
+++ jk.js
@@ -0,0 +1,14 @@
+
+var lang=1;
+$('.eng').show();
+$('.fin').hide();
+function selectFin(){
+$('.eng').hide();
+$('.fin').show();
+}
+function selectEn(){
+$('.eng').show();
+$('.fin').hide();
+}
+
+
