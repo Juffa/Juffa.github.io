a7e4875d72cd7553db396904fae57fc2bbc04e10
Juha Kaikko <jkk79.jee@gmail.com>
2020-05-20 03:25:21 +0300

    added Finnish translations and a toggle for it.

--- jk.js
+++ jk.js
@@ -1,14 +1,23 @@
 
-var lang=1;
-$('.eng').show();
-$('.fin').hide();
-function selectFin(){
-$('.eng').hide();
-$('.fin').show();
-}
-function selectEn(){
-$('.eng').show();
-$('.fin').hide();
-}
+  jkvalue=localStorage.getItem("jklanguage");
+
+  if(jkvalue=="0"||null)
+  {
+    $('.eng').show();
+    $('.fin').hide();
+  }else{
+    $('.eng').hide();
+    $('.fin').show();
+  }
+    function selectFin(){
+      localStorage.setItem("jklanguage","1");
+      $('.eng').hide();
+      $('.fin').show();
+      }
+    function selectEn(){
+      localStorage.setItem("jklanguage","0");
+      $('.eng').show();
+      $('.fin').hide();
+      }
 
 
